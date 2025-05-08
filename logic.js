1de65ee0c01e553b92c4a78580b17b73c7b6c242
LucaCallus <lucacallus@gmail.com>
2025-05-08 19:59:38 +0200

    more funcionality

--- logic.js
+++ logic.js
@@ -1,21 +1,42 @@
 let money = 0;
-let rate = [1, 1, 1, 1, 1, 1];
-let cost = [50, 50, 50, 50, 50, 50];
+let rate = 1;
+let rates = [1, 0, 0, 0, 0, 0];
+let cost = [10, 75, 100, 125, 150, 200];
+let levels = [1, 0, 0, 0, 0, 0];
 
-function count(){
+function countAuto(){
     money += rate;
     document.querySelector("#money").innerHTML = money;
 }
 
-setInterval(count, 1000)
+function count(){
+    money += 1; // expsenive upgrade to be able to upgrade this
+    document.querySelector("#money").innerHTML = money;
+}
 
-function upgrade(index, costNo){
+setInterval(countAuto, 1000);
+
+function upgrade(index){
     if(money >= cost[index]){
-        money -= cost[index];
-        rate[index] *= 2 // multiply rate by 2 (2^n)
-        document.querySelector("#rate").innerHTML = rate;
+        money -= cost[index]; // decrease money
+
+        // Update Rate
+        if(levels[index]==0) rates[index] = 2; // set rate to 2
+        else rates[index] *= 2; // multiply rate by 2 (2^n)
+        document.querySelector(`#rate${index+1}`).innerHTML = rates[index];
+
+        // Update Level
+        levels[index] += 1;
+        document.querySelector(`#level${index+1}`).innerHTML = levels[index];
+
+        // Update Cost
         cost[index] = Math.round(2.5 * cost[index]); // multiply cost by 2.5 (2.5^n)
-        document.querySelector(`${costNo}`).innerHTML = cost[index];
+        document.querySelector(`#cost${index+1}`).innerHTML = cost[index];
+
+        // Update overall rate
+        rate = rates.reduce((a, b) => a+b, 0);
+        document.querySelector("#rate").innerHTML = rate;
+        
     }
     else alert("Not Enough Money");
 }
@@ -27,7 +48,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Add onclick property for each mechanic
     for(let i=0; i<6; i++){
-        document.querySelector(`#mechanic${i+1}`).onclick = () => upgrade(i, `#cost${i+1}`);
+        document.querySelector(`#mechanic${i+1}`).onclick = () => upgrade(i);
     }
     // mechanic (for each one) - need to: increase cost, increase $/s, check if player has enough money
 
